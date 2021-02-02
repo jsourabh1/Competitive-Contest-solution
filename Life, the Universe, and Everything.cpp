630056da5839cb41ae14ff1ad5aee09f184bebef
saurabh jain <65993641+jsourabh1@users.noreply.github.com>
2021-02-02 11:51:20 +0530

    DSA Learning Series - Contest 1
    https://www.codechef.com/LRNDSA01/problems/TEST/

---
+++ Life, the Universe, and Everything.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    while(1){
+        int n;
+        cin>>n;
+        if(n==42){
+            break;
+        }
+        else{
+            cout<<n<<endl;
+        }
+    }
+	return 0;
+	
+}
