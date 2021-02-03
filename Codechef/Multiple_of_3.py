09767f93aef0585d0fcb780fd90c489b94120aaa
saurabh jain <65993641+jsourabh1@users.noreply.github.com>
2021-02-03 08:58:57 -0800

    DSA Learning series

--- Codechef/Multiple_of_3.py
+++ Codechef/Multiple_of_3.py
@@ -1,55 +1,31 @@
-def solve(n,d1,d2):
-    number = str(d1) + str(d2)
-    res = n - 3
-    string = ""
-    for i in range(5):
-        j = int((math.pow(2, i) * int(d1 + d2)) % 10)
-       
-        if i > 0:
-            string += str(j)
-        number += str(j)
-
-    #print(number,string)
-        
-        
-    if n>7:
-
-        sum2 = int(number[0]) + int(number[1]) + int(number[2]) 
-                
-        #print(sum2)
-        if res % 4 == 0:
-
-            sum2 += 20 * (res // 4)
-        else:
-            index = res % 4
-            #print(index,20 * (res // 4))
-            sum2 += 20 * (res // 4)
-            #print(sum2)
-            stri = string[:index]
-            for i in stri:
-                sum2 += int(i)
-           # print(sum2)
-    else:
-        sum2=0
-        for i in number[:n]:
-            sum2+=int(i)
-
-    #print(sum2)
-    if sum2 % 3 == 0:
-        print("YES")
-    else:
-        print("NO")
-    
-    
-    return
 
 
-
-import math
+from math import pow
 
 for _ in range(int(input())):
 
-    n, d1, d2 = map(int, input().split())
+    n, a, b = map(int, input().split())
+    p=a+b
+    r = (pow(2,0)*(p));  r = r % 10;
+    s = (pow(2,1)*(p));  s = s % 10;
+    t = (pow(2,2)*(p));  t = t % 10;
+    u = (pow(2,3)*(p));  u = u % 10;
+    v = (pow(2,4)*(p));  v = v % 10;
+    val = (s+t+u+v);
+    rem = (k-3)%4;
+    ans = (k-3)/4;
+    sp = sp + p + r;
+    if(rem == 1)  sp = sp + s;
+    if(rem == 2)  sp = sp + t + s;
+    if(rem == 3)  sp = sp + u + s + t;
+    if(sp % 3 == 0):
+        
+        print("YES")
+    else :
+        print("NO")
+
     
-    solve(n,d1,d2)
+    
+    
+    
     
