a2ba2672ba1a1311daf34f39ca2b0fe4af83711f
saurabh jain <65993641+jsourabh1@users.noreply.github.com>
2021-03-04 00:50:55 +0530

    Add files via upload

---
+++ Codechef/Feburary_lunchtime/coprime.py
@@ -0,0 +1,18 @@
+
+#first question 
+	#question link:-https://www.codechef.com/LTIME93B/problems/COPRIMEX
+	# you have to simply print highest prime no from the give range so 
+	# i founded the largeset prime is 10005011
+	#python solution 
+
+
+for i in range(int(input())):
+
+	a,b=map(int,input().split())
+	print(10005011)
+
+
+
+
+
+
