5317f68aeb6b3ded6f463b28de1eaef5c6680dce
saurabh jain <65993641+jsourabh1@users.noreply.github.com>
2021-06-19 10:48:56 +0530

    Codeforces #726 (DIV-2)

---
+++ question3_726.py
@@ -0,0 +1,231 @@
+#DaRk DeveLopeR
+'''
+You are a game designer and want to make an obstacle course. The player will walk from left to right. You have n heights of mountains already selected and want to arrange them so that the absolute difference of the heights of the first and last mountains is as small as possible.
+
+In addition, you want to make the game difficult, and since walking uphill or flat is harder than walking downhill, the difficulty of the level will be the number of mountains i (1≤i<n) such that hi≤hi+1 where hi is the height of the i-th mountain. You don't want to waste any of the mountains you modelled, so you have to use all of them.
+
+From all the arrangements that minimize |h1−hn|, find one that is the most difficult. If there are multiple orders that satisfy these requirements, you may find any.
+
+input
+2
+4
+4 2 1 2
+2
+3 1
+output
+2 4 1 2 
+1 3
+
+question link:-https://codeforces.com/contest/1537/problem/C
+'''
+
+import sys
+
+
+#taking input as string 
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+inp = lambda: list(map(int,sys.stdin.readline().rstrip("\r\n").split()))
+mod = 10**9+7; Mod = 998244353; INF = float('inf')
+#______________________________________________________________________________________________________
+import math
+from bisect import *
+from heapq import *
+from collections import defaultdict as dd
+from collections import OrderedDict as odict
+from collections import Counter as cc
+from collections import deque
+from itertools import groupby
+sys.setrecursionlimit(20*20*20*20+10) #this is must for dfs
+
+
+#question link:-
+def solve():
+
+	n=takein()
+
+
+	arr=takeiar()
+
+
+	arr.sort()
+
+	index=0
+	minn=sys.maxsize
+
+	for i in range(n-1):
+
+		if minn>abs(arr[i]-arr[i+1]):
+			index=i
+
+			minn=abs(arr[i]-arr[i+1])
+
+	# print(arr)
+
+	if n==2:
+
+		print(arr[0],arr[1])
+		return 
+	for i in range(index+1,n):
+		print(arr[i],end=" ")
+	for i in range(0,index+1):
+		print(arr[i],end=" ")
+
+	print()
+
+	return 
+
+
+
+	
+
+
+
+
+def main():
+    global tt
+    if not ONLINE_JUDGE:
+        sys.stdin = open("input.txt","r")
+        sys.stdout = open("output.txt","w")
+    t = 1
+    t = takein()
+    #t = 1
+    for tt in range(1,t + 1):
+        solve()
+    if not ONLINE_JUDGE:
+        print("Time Elapsed :",time.time() - start_time,"seconds")
+        sys.stdout.close()
+
+#---------------------- USER DEFINED INPUT FUNCTIONS ----------------------#
+def takein():
+    return (int(sys.stdin.readline().rstrip("\r\n")))
+
+
+# input the string
+
+def takesr():
+    return (sys.stdin.readline().rstrip("\r\n"))
+
+
+# input int array
+def takeiar():
+    return (list(map(int, sys.stdin.readline().rstrip("\r\n").split())))
+
+
+# input string array
+def takesar():
+    return (list(map(str, sys.stdin.readline().rstrip("\r\n").split())))
+
+
+# innut values for the diffrent variables
+def takeivr():
+    return (map(int, sys.stdin.readline().rstrip("\r\n").split()))
+
+
+def takesvr():
+    return (map(str, sys.stdin.readline().rstrip("\r\n").split()))
+
+ 
+ 
+#------------------ USER DEFINED PROGRAMMING FUNCTIONS ------------------#
+
+
+
+def ispalindrome(s):
+
+	return s==s[::-1]
+ 
+def invert(bit_s):
+
+
+	  
+	# convert binary string 
+	# into integer
+	temp = int(bit_s, 2)
+	  
+	# applying Ex-or operator
+	# b/w 10 and 31
+	inverse_s = temp ^ (2 ** (len(bit_s) + 1) - 1)
+	  
+	# convert the integer result 
+	# into binary result and then 
+	# slicing of the '0b1' 
+	# binary indicator
+	rslt = bin(inverse_s)[3 : ]
+
+	return str(rslt) 	  
+
+def counter(a):
+    q = [0] * max(a)
+    for i in range(len(a)):
+        q[a[i] - 1] = q[a[i] - 1] + 1
+    return(q)
+ 
+def counter_elements(a):
+    q = dict()
+    for i in range(len(a)):
+        if a[i] not in q:
+            q[a[i]] = 0
+        q[a[i]] = q[a[i]] + 1
+    return(q)
+ 
+def string_counter(a):
+    q = [0] * 26
+    for i in range(len(a)):
+        q[ord(a[i]) - 97] = q[ord(a[i]) - 97] + 1
+    return(q)
+ 
+def factorial(n,m = 1000000007):
+    q = 1
+    for i in range(n):
+        q = (q * (i + 1)) % m
+    return(q)
+ 
+def factors(n):
+    q = []
+    for i in range(1,int(n ** 0.5) + 1):
+        if n % i == 0: q.append(i); q.append(n // i)
+    return(list(sorted(list(set(q)))))
+ 
+def prime_factors(n):
+    q = []
+    while n % 2 == 0: q.append(2); n = n // 2
+    for i in range(3,int(n ** 0.5) + 1,2):
+        while n % i == 0: q.append(i); n = n // i
+    if n > 2: q.append(n)
+    return(list(sorted(q)))
+ 
+def transpose(a):
+    n,m = len(a),len(a[0])
+    b = [[0] * n for i in range(m)]
+    for i in range(m): 
+        for j in range(n): 
+            b[i][j] = a[j][i]
+    return(b)
+ 
+def power_two(x):
+    return (x and (not(x & (x - 1))))
+ 
+def ceil(a, b):
+    return -(-a // b)
+
+
+
+def seive(n):
+	a = [1]
+	prime = [True for i in range(n+1)] 
+	p = 2
+	while (p * p <= n): 
+	    if (prime[p] == True): 
+	        for i in range(p ** 2,n + 1, p): 
+	            prime[i] = False
+	    p = p + 1
+	for p in range(2,n + 1): 
+	    if prime[p]: 
+	        a.append(p)
+	return(a)
+#-----------------------------------------------------------------------#
+ONLINE_JUDGE = __debug__
+if ONLINE_JUDGE:
+    input = sys.stdin.readline
+    
+main()
