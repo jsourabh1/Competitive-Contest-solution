d6c514f323062c520962517f7e609a42c7b9201f
Saurabh Jain <jsourabh861@gmail.com>
2021-10-12 06:46:32 +0530

    Create october10.py

---
+++ Leetcode/october_challenge_2021/october10.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rangeBitwiseAnd(self, m, n):
+        i = 0
+        while m != n:
+            m >>= 1
+            n >>= 1
+            i += 1
+        return n << i
+
+        
+        
+        
