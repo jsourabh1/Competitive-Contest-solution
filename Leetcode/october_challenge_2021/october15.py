79a8b1f6b4ed0d5b037ea5bd6648215e6da8a83a
Saurabh Jain <jsourabh861@gmail.com>
2021-10-19 07:17:19 +0530

    october_15 buy and sell III

---
+++ Leetcode/october_challenge_2021/october15.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        
+        if len(prices) < 2:
+            return 0
+        sell, buy, prev_sell, prev_buy = 0, -prices[0], 0, 0
+        for price in prices:
+            prev_buy = buy
+            buy = max(prev_sell - price, prev_buy)
+            prev_sell = sell
+            sell = max(prev_buy + price, prev_sell)
+        return sell
